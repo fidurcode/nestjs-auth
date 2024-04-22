8b0a3be2c7bf3c0ff6cdae2ef07b340a65e7ef02
fidurcode <dev@fidurcode.pl>
2024-04-22 21:07:06 +0200

    Feat: Simply swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,30 @@ import { AppModule } from './app.module';
 import { configDotenv } from 'dotenv';
 import * as process from 'node:process';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 configDotenv();
 
+function getSwaggerConfig(app: INestApplication<AppModule>) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('AuthApi')
+    .setDescription('The auth API description')
+    .setVersion('1.0')
+    .addTag('open-api')
+    .addBearerAuth()
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+  SwaggerModule.setup('open-api', app, document);
+}
 async function bootstrap() {
   const app: INestApplication<AppModule> = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  getSwaggerConfig(app);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
